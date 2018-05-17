6bcb8848d01e1b33e06b0918ee6530c132d4810b
Binsheng Liu <441707+binshengliu@users.noreply.github.com>
2018-05-17 16:24:16 +1000

    update polybar script

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -4,19 +4,18 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -u $USER -x polybar >/dev/null; do sleep 1; done
 
 # Stole from here:
 # https://github.com/jaagr/polybar/issues/763#issuecomment-331604987
 if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload i3wmthemer_bar &
-  done
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+	export MONITOR=$m
+	MONITOR=$m polybar --reload i3wmthemer_bar &
+	unset MONITOR
+    done
 else
-  polybar --reload i3wmthemer_bar &
+    polybar --reload i3wmthemer_bar
 fi
-
-polybar i3wmthemer_bar &
-# polybar bar2 &
 
 echo "Bars launched..."
